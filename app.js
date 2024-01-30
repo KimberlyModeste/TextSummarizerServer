17d75cb2c96293e31e53728efa033fe3e763a807
KimberlyModeste <54868542+KimberlyModeste@users.noreply.github.com>
2024-01-30 14:24:23 -0500

    Added options for cors

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 	next();
 })
-// app.use("Access-Control-Allow-Origin", "*")
-// app.use("Access-Control-Allow-Methods", "POST, GET, PUT")
-// app.use("Access-Control-Allow-Methods", "POST, GET, PUT")
+app.options('*', cors())
 
 //Uses the python summary file
 app.post("/summary", (req, res) => {
