f178a09944c231a2fd952bd46b884967269ac55c
KimberlyModeste <54868542+KimberlyModeste@users.noreply.github.com>
2024-01-31 13:37:52 -0500

    Seeing if moving bullet would work with TextSum

--- app.js
+++ app.js
@@ -31,8 +31,7 @@ app.post("/summary", (req, res) => {
 
 	if (len === 0)
 	{
-		child = spawn('./pyAPI/bullet.py',[req.body.text])
-		// child = spawn('python', ['TextSummarizerServer/pyAPI/bullet.py', req.body.text])
+		child = spawn('python', ['TextSummarizerServer/pyAPI/bullet.py', req.body.text])
 	}
 	else
 	{
